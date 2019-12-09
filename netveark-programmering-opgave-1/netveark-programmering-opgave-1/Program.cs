21958b0ee44e862e08a6b97c243144ed2d868218
Oliver <OIOl.skp@edu.mercantec.dk>
2019-12-09 09:27:55 +0100

    Completed tasks 1-5
    Network programming tasks 1-5 completed

--- netveark-programmering-opgave-1/netveark-programmering-opgave-1/Program.cs
+++ netveark-programmering-opgave-1/netveark-programmering-opgave-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace netveark_programmering_opgave_1
 {
@@ -6,7 +7,30 @@ namespace netveark_programmering_opgave_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there!");
+            // =========================== Converting with ASCII =============================
+            string Tekst1 = "O hello there hæhæ!";
+
+            // Converting string Tekst1 to bytes
+            var Tekst1InBytes = Encoding.ASCII.GetBytes(Tekst1);
+
+            // Converting bytes back to a string
+            string Tekst1ByteConvert = Encoding.ASCII.GetString(Tekst1InBytes);
+
+            // Now writing the code
+            Console.WriteLine(Tekst1ByteConvert);
+
+
+            // ======================= Converting with UTF8 ================================000
+            string Tekst2 = "O hello there hæhæ!";
+
+            // Converting string Tekst1 to bytes
+            var Tekst2InBytes = Encoding.UTF8.GetBytes(Tekst2);
+
+            // Converting bytes back to a string
+            string Tekst2ByteConvert = Encoding.UTF8.GetString(Tekst2InBytes);
+
+            // Now writing the code
+            Console.WriteLine(Tekst2ByteConvert);
         }
     }
 }
