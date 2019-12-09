d24aba649d5bbbe281bca34b1ed0cfd341bbf266
Dolphus <57088318+oliv6000@users.noreply.github.com>
2019-12-09 09:01:52 +0100

    Task 1. c# file
    Had to upload through github, because i had no idea where to save the file yet

---
+++ netveark-programmering-opgave-1/netveark-programmering-opgave-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netveark_programmering_opgave_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
